1512159cc3ca7dcfda8698fa61852bf2c40c9fdd
Juan Moreno <ext_jcmorenos@falabella.cl>
2020-03-04 22:14:05 -0300

    [INIT]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HijoComponent } from './components/contador/hijo/hijo.component';
+import { NietoComponent } from './components/contador/nieto/nieto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HijoComponent,
+    NietoComponent
   ],
   imports: [
     BrowserModule
